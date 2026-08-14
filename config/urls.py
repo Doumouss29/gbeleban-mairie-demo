b40ed35ead6255709ec417cf58290328ddc64d97
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-14 06:29:16 +0200

    Expose le tableau de bord statistiques

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pilotage-projets/", include("pilotage.urls")),
+    path("analytics/", include("siteanalytics.urls")),
     path("", include("portal.urls")),
 ]
