b0e0520a8fb7e021e635066bc121562d366d0f3e
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-14 06:27:08 +0200

    Configure le module de statistiques

---
+++ siteanalytics/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SiteanalyticsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "siteanalytics"
+    verbose_name = "Statistiques du portail"
