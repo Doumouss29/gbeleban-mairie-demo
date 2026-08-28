3b4066335ae8a24a5f6e95f15204e66df2a82bf3
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-28 11:43:36 +0200

    Use map template with address search

--- portal/public_map_views.py
+++ portal/public_map_views.py
@@ -5,7 +5,7 @@ from .public_map_data import get_public_map_geojson
 
 
 def map_public(request):
-    return render(request, "portal/map.html")
+    return render(request, "portal/map_search.html")
 
 
 def public_map_geojson(request):
